df477c740a5eded2b0ea7612c22918843bd47ab3
Lucas Coimbra <69476182+Nikodemos0612@users.noreply.github.com>
2021-09-22 16:17:15 -0300

    Initial commit

---
+++ app/src/main/java/br/unicamp/pull_it/MainActivity.java
@@ -0,0 +1,14 @@
+package br.unicamp.pull_it;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
